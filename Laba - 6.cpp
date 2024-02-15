45cee758ae8e064c2705373e69c2c210b1b77c7b
SlavaEliz <148941881+SlavaEliz@users.noreply.github.com>
2024-02-16 01:55:49 +0500

    Create Laba - 6.cpp

---
+++ Laba - 6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    int i, n, j, p = 0;
+    string str = "no info";
+    getline (cin, str);
+    
+    while (str[n] != '.') {
+        j++;
+        n++;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        if (str[i] == ' ') {
+            a = i; 
+            p++
+        }
+        else if (s)
+    }
+    cout << n << endl;
+    cout << str;
+    return 0;
+}
