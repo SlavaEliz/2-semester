861954cb82c089bbb222c3336e23d7a572430c4b
SlavaEliz <148941881+SlavaEliz@users.noreply.github.com>
2024-02-16 01:21:42 +0500

    Create 7.2.cpp

---
+++ 7.2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int n = 3
+
+int days (int n, ...) {
+    int 
+    days = 
+    
+}
+
+int main () {
+    int day, month, year, a;
+    
+    cout << "Введите день" << endl;
+    cin >> day;
+    cout << "Введите месяц" << endl;
+    cin >> month;
+    cout << "Введите год" << endl;
+    cin >> year;
+    
+    a = days (3, 'day', 'month', 'year');
+    
+    
+    
+    days (3, day)
+}
+
+
+
+    cout << "Введите день" << endl;
+    cin >> day;
+    cout << "Введите месяц" << endl;
+    cin >> month;
+    cout << "Введите год" << endl;
+    cin >> year;
