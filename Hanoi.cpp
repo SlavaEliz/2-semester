310dd9c35aa2c68f5d8c12b70229501662c0a293
SlavaEliz <148941881+SlavaEliz@users.noreply.github.com>
2024-02-16 15:07:30 +0500

    Update Hanoi.cpp

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
+static int i = 1;
+
 int han(int n, int start, int temp, int point) {
 if (n > 0) {
 han (n - 1, start, point, temp);
-cout << start << " => " << temp << endl;
+cout << start << " => " << temp << "  " << i++ << endl;
 han (n - 1, point, temp, start);
 }
 return 0;
